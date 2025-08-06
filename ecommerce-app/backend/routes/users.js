6bb3a870755cfa655b43bd3bf59106c2f5bb25a4
Sina-ramezani <sina.software.dev@gmail.com>
2025-08-07 01:13:41 +0330

    Delete ecommerce-app

--- ecommerce-app/backend/routes/users.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { protect } = require('../middleware/auth');
-
-// Basic user routes placeholder
-// Additional user management routes can be added here
-
-module.exports = router;
